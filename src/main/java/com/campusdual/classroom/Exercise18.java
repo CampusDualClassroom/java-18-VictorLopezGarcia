2bc7b8011c2d7962cdf405ee77fb4683b0138861
VictorLopezGarcia <victor.lopezg@udc.es>
2025-05-22 12:30:39 +0200

    Ejercicio18

--- src/main/java/com/campusdual/classroom/Exercise18.java
+++ src/main/java/com/campusdual/classroom/Exercise18.java
@@ -1,8 +1,31 @@
 package com.campusdual.classroom;
 
+import com.campusdual.util.Utils;
+
+
+
 public class Exercise18 {
 
 	public static void main(String[] args) {
+		int numero = Utils.integer("Ingresa los números de posiciones del array:");
+		showInlineArray(createAndInitializeArray(numero));
+	}
 
+	public static int[] createAndInitializeArray(int num) {
+		int[] intArray = new int[num];
+		for (int i = 0; i < num; i++) {
+			intArray[i] = i+1;
+		}
+		return intArray;
+	}
+
+	public static void showInlineArray(int[] intArray) {
+		for (int i = 0; i < intArray.length; i++) {
+			System.out.print(intArray[i]);
+			if (i < intArray.length - 1) {
+				System.out.print(" ");
+			}
+		}
+		System.out.println();
 	}
 }
